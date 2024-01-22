46c556aee3e58705cd51e5fc4583ea83b55796b2
AnilBurcu <anlbrc95@gmail.com>
2024-01-22 13:26:26 +0300

    API Update

---
+++ app.js
@@ -0,0 +1,12 @@
+/* 
+! browsers storage
+local storage session storage, 
+*/
+
+// How to add data localstorage
+localStorage.setItem("kullaniciAdi","Anl");
+
+// How to gett data from localstorage
+const kullaniciAdi = localStorage.getItem("kullaniciAdi")
+console.log(kullaniciAdi)
+
--- main.js
+++ main.js
@@ -2,59 +2,83 @@ const chatInput = document.querySelector("#chat-input");
 const sendButton = document.querySelector("#send-btn");
 const chatContainer = document.querySelector(".chat-container");
 const themeButton = document.querySelector("#theme-btn");
+const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
 
 const API_KEY = "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"
 
+const loadDataFromLocalStorage = () => {
+  //checks theme and apply existing theme
+  const themeColor = localStorage.getItem("theme-color")
+  document.body.classList.toggle("light-mode", themeColor === "light_mode");
+  //update theme in localstorage
+  localStorage.setItem("theme-color", themeButton.innerText);
+  themeButton.innerText = document.body.classList.contains("light-mode")
+    ? "dark_mode"
+    : "light_mode"
+
+    //get chat from localstorage or apply default content
+    const defaultText = `
+    <div class="default-text>
+    <h1>ChatGPT Clone<h1>
+    <div>
+    `;
+    chatContainer.innerHTML = localStorage.getItem("all-chats") || defaultText;
+
+    //swipe bottom of chat
+    chatContainer.scrollTo(0,chatContainer.scrollHeight);
+}
+loadDataFromLocalStorage();
+
 const createElemet = (html, className) => {
-    // Creating new div with chat class
-    // Set divs html contents
-    const chatDiv = document.createElement("div")
-    chatDiv.classList.add("chat", className)
-    chatDiv.innerHTML = html;
-    return chatDiv;
+  // Creating new div with chat class
+  // Set divs html contents
+  const chatDiv = document.createElement("div")
+  chatDiv.classList.add("chat", className)
+  chatDiv.innerHTML = html;
+  return chatDiv;
 }
 
 const getChatResponse = async (incomingChatDiv) => {
-    const API_URL = "https://api.openai.com/v1/completions";
-    const pElement = document.createElement("p");
-    // Define API Request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${API_KEY}`,
-        },
-        body: JSON.stringify({
-            model: "text-davinci-003",
-            prompt: userText,
-            max_tokens: 2048,
-            temperature: 0.2,
-            n: 1,
-            stop: null,
-        }),
-    };
-    //
-    try {
-        const response = await (await fetch(API_URL, requestOptions)).json();
-        console.log(response);
-        pElement.textContent = response.choices[0].text.trim();
-    } catch (error) {
-        console.log(error);
-        pElement.textContent = "Opppss";
-        pElement.style.color = "red";
-    }
-    incomingChatDiv.querySelector(".typing-animation").remove();
-    incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
-    chatContainer.scrollTo(0, chatContainer.scrollHeight);
-    localStorage.setItem("all-chats", chatContainer.innerHTML);
+  const API_URL = "https://api.openai.com/v1/completions";
+  const pElement = document.createElement("p");
+  // Define API Request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: userText,
+      max_tokens: 2048,
+      temperature: 0.2,
+      n: 1,
+      stop: null,
+    }),
+  };
+  //
+  try {
+    const response = await (await fetch(API_URL, requestOptions)).json();
+    console.log(response);
+    pElement.textContent = response.choices[0].text.trim();
+  } catch (error) {
+    console.log(error);
+    pElement.textContent = "Opppss";
+    pElement.style.color = "red";
+  }
+  incomingChatDiv.querySelector(".typing-animation").remove();
+  incomingChatDiv.querySelector(".chat-details").appendChild(pElement);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  localStorage.setItem("all-chats", chatContainer.innerHTML);
 
 
 };
 
 const showTypingAnimation = () => {
-    const html = `
+  const html = `
       <div class="chat-content">
       <div class="chat-details">
         <img src="images/chatbot.jpg" alt="chat-images" />
@@ -67,16 +91,16 @@ const showTypingAnimation = () => {
       <span class="material-symbols-outlined"> content_copy </span>
     </div>
       `;
-    const incomingChatDiv = createElemet(html, "incoming");
-    chatContainer.appendChild(incomingChatDiv);
-    chatContainer.scrollTo(0, chatContainer.scrollHeight);
-    getChatResponse(incomingChatDiv);
+  const incomingChatDiv = createElemet(html, "incoming");
+  chatContainer.appendChild(incomingChatDiv);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  getChatResponse(incomingChatDiv);
 };
 
 const handleoutGoingChat = () => {
-    userText = chatInput.value.trim(); // trim -> Bastaki ve sondaki bosluklari atar(api hata vermemesi icin)
-    if (userText == null) return; // Input bos ise calismasin
-    const html = `<div class="chat-content">
+  userText = chatInput.value.trim(); // trim -> Bastaki ve sondaki bosluklari atar(api hata vermemesi icin)
+  if (userText == null) return; // Input bos ise calismasin
+  const html = `<div class="chat-content">
     <div class="chat-details">
       <img src="images/user.jpg" alt="user-image" />
       <p>
@@ -85,23 +109,43 @@ const handleoutGoingChat = () => {
       
     </div>
   </div>`;
-    const outGoingChatDiv = createElemet(html, "outgoing");
-    outGoingChatDiv.querySelector("p").textContent = userText;
-    document.querySelector(".default-text")?.remove();
-    chatContainer.appendChild(outGoingChatDiv);
-    chatContainer.scrollTo(0, chatContainer.scrollHeight);
+  const outGoingChatDiv = createElemet(html, "outgoing");
+  outGoingChatDiv.querySelector("p").textContent = userText;
+  document.querySelector(".default-text")?.remove();
+  chatContainer.appendChild(outGoingChatDiv);
+  chatContainer.scrollTo(0, chatContainer.scrollHeight);
 
-    setTimeout(showTypingAnimation, 500);
-    // ${} -> For dynamic structure; swift /() 
+  setTimeout(showTypingAnimation, 500);
+  // ${} -> For dynamic structure; swift /() 
 
 };
 
 themeButton.addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-    themeButton.innerText = document.body.classList.contains("light-mode")
-        ? "dark_mode"
-        : "light_mode";
+  document.body.classList.toggle("light-mode");
+  themeButton.innerText = document.body.classList.contains("light-mode")
+    ? "dark_mode"
+    : "light_mode";
+});
+
+deleteButton.addEventListener("click", () => {
+  if (confirm("Are you sure?")) {
+    localStorage.removeItem("all-chats");
+    loadDataFromLocalStorage();
+  }
 });
 
 
+
+chatInput.addEventListener("input", () => {
+  chatInput.style.height = `${initialHeight}px`;
+  chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
+chatInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    handleoutGoingChat();
+  }
+});
+
 sendButton.addEventListener("click", handleoutGoingChat);
+
